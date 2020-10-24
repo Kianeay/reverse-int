6354fceb992bb19823f2844fb8c355b4e4efcc01
Kianeay <70838054+Kianeay@users.noreply.github.com>
2020-10-24 13:47:34 +0300

    end: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n = n + "";
+    let res = n.split("").reverse().join("");
+    
+if (res[3] === '-') {
+    let result = `${res[0]}${res[1]}${res[2]}`;
+    return result;
+} else {
+    return res;
+}
+
 }
